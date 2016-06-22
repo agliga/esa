b60414972074a31da678faf5dd33a312a0e871b4
Andrew Gliga <agliga@ebay.com>
2016-06-22 13:41:37 -0700

    Added express server to serve nodejs on a single application

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-var http = require ('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (request, res) {
-
+app.get('/query', (request, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain',
-                      'Access-Control-Allow-Origin': "*",
-                      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'});
+    'Access-Control-Allow-Origin': "*",
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'});
   var params = "{\"operation\":\"read\",\"tableName\":\"LambdaTable\",\"payload\":{\"Key\":{\"Id\":\"1\"}}}";
 
   var options = {
-    host: 'h8c6gba5x8.execute-api.us-east-1.amazonaws.com',
-    path: '/prod/LambdaFunctionOverHttps',
+    host: 'znzdfoyhaa.execute-api.us-east-1.amazonaws.com',
+    path: '/prod/eBayOnAlexa',
     port: '443',
     method: 'POST'
-   };
+  };
 
   var https = require('https');
   callback = function (response) {
     //console.log(response.statusCode);
     var str = ' ';
     response.on('data', function (chunk) {
-       str += chunk;
+      str += chunk;
     });
 
     response.on('end', function () {
@@ -28,16 +28,20 @@ http.createServer(function (request, res) {
       //console.log(str);
       res.end(str);
     });
-  }  
+  }
 
-   try {
-     var req = https.request(options, callback);
-     req.write(params);
-     req.end();
-   } catch (e) {
-     console.error(e.stack);
-   } 
+  try {
+    var req = https.request(options, callback);
+    req.write(params);
+    req.end();
+  } catch (e) {
+    console.error(e.stack);
+  }
 
-}).listen(8081);
+});
 
-console.log('Server running at http://127.0.0.1:8081/');
+app.use(express.static('public'));
+
+app.listen(8081, function () {
+  console.log('Server running at http://127.0.0.1:8081/');
+});
